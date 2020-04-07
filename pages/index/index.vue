808a062a8c4db31209e6d72a9cd6172d351e365d
wangxiaoxing <995107408@qq.com>
2020-04-07 10:04:29 +0800

    add

---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<web-view src="https://petkingdompremium.com">
+	</web-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
